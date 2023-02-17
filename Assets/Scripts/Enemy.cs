1be73477626db41407a5ed366f41cf9f25252002
Ema <emanu.87@gmail.com>
2023-02-17 12:01:53 -0600

    Adding some assets and scripts

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField]
+    private int _speed = 3;
+    [SerializeField]
+    private int _life = 10;
+    [SerializeField]
+    private int _experience = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,17 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // IA to move closer to the player
+        Player player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        Vector2 direction = player.transform.position - transform.position;
+        // 0.1 since position.x is never 0
+        if(direction.x > 0.1)
+        {
+            transform.Translate(_speed * Time.deltaTime, 0, 0);
+        } 
+       if (direction.x < -0.1)
+        {
+            transform.Translate(-_speed * Time.deltaTime, 0, 0);
+        }
     }
 }
